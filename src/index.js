4c7fbc9212d24ea1386f9c12053827b33e3f3f4b
Lorenz B <a11936358@unet.univie.ac.at>
2022-05-29 15:03:35 +0200

    Session Tracking

--- src/index.js
+++ src/index.js
@@ -3,21 +3,25 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env['TELEGRAM_BOT_TOKEN'];
 const bot = new TelegramBot(token, {polling: true});
 
-
 const state = {
-	started:false
+	started:false,
+    current_chats:[]
 };
 
 bot.onText(/\/start/, (msg) => {
-    console.log(msg);
     state.started = true;
     bot.sendMessage(msg.chat.id, 'Started. Possible commands are: ...');
     console.log('started');
+    state.current_chats.push(msg.from.id);
 });
 
 bot.on('message', message => {
-    if(state.started === true){
+    if(state.current_chats.includes(message.from.id) && state.started === true){
         bot.sendMessage(message.chat.id, 'Answering now!');
+    }else if(message.text.startsWith('/')){
+    }
+    else{
+        bot.sendMessage(message.chat.id, 'Please start the bot with /start!');
     }
 });
 
