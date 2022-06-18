824d1ad92fc71cfa963732758987bca728155048
Matus Mrekaj <a12142038@unet.univie.ac.at>
2022-06-18 18:50:04 +0200

    update bot

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 
 var natural = require('natural');
-//natural.PorterStemmer.attach();
 var TfIdf = natural.TfIdf;
-
 
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env['TELEGRAM_BOT_TOKEN'];
@@ -18,8 +16,48 @@ const tokenizedArticles = GDPR.map(article => {
     return natural.PorterStemmer.tokenizeAndStem(article.matchText);
 });
 
+const handleCommands = (message) => {
+    if (['/start', '/stop'].includes(message.text)) { /* do nothing */ }
+    else if (message.text === "/help") {
+        let output = [
+            "This bot will try to answer your description of *GDPR concerns* by returning relavant *GDPR articles*.",
+            "Subsequently if requested it will also provide relevant *AWS article* for the implementation of that *GDPR concern*.",
+            "",
+            "",
+            "To start interacting with the bot type: `/start` and then describe your GDPR concern.",
+            "At any time you can stop the conversation with the bot by typing: `/stop`.",
+            "",
+            "",
+            "Due to the enormity of GDPR articles and information this bot may not capture all of them.",
+            "Thus, in that case the following website can be used to further research the topic:",
+            "```\t\t • Relevant GDPR (latest) information:``` https://gdpr.eu/",
+            "",
+            "If we don't provide the AWS solution for your described GDPR solution you can contanct AWS directly:",
+            "```\t\t • AWS GDPR complaince contact form:``` https://pages.awscloud.com/compliance-contact-us.html"
+        ];
+
+        bot.sendMessage(message.chat.id, output.join('\n'), {
+            parse_mode: 'Markdown',
+            disable_web_page_preview: true,
+        });
+    } else {
+        let output = [
+            "Only the following commands are supported",
+            " • `/help`  - explains how the bot works and provides some additional information",
+            " • `/start`  - starts the interaction with the bot",
+            " • `/stop`  - stops the interaction with the bot",
+        ];
+        bot.sendMessage(message.chat.id, output.join('\n'), {
+            parse_mode: 'Markdown',
+            disable_web_page_preview: true,
+        });
+    }
+}
+
 bot.on('message', message => {
-    if (state.current_chats.includes(message.from.id)) {
+    if (message.text.startsWith('/')) {
+        handleCommands(message);
+    } else if (state.current_chats.includes(message.from.id)) {
         console.log(tokenizedArticles);
 
         var tfidf = new TfIdf();
@@ -31,16 +69,16 @@ bot.on('message', message => {
 
         var matches = [];
 
-        tfidf.tfidfs(tokenizedAndStemmedMsg, function(i, measure) {
-            matches.push({i,measure});
+        tfidf.tfidfs(tokenizedAndStemmedMsg, function (i, measure) {
+            matches.push({ i, measure });
             console.log('document #' + i + ' is ' + measure);
         })
 
-        matches = matches.sort(function(x,y){
-            if(x['measure']>y['measure']){
+        matches = matches.sort(function (x, y) {
+            if (x['measure'] > y['measure']) {
                 return -1;
             }
-            if(x['measure']<y['measure']){
+            if (x['measure'] < y['measure']) {
                 return 1;
             }
             return 0;
@@ -53,17 +91,17 @@ bot.on('message', message => {
         if (matches[0]['measure'] === 0) {
             bot.sendMessage(message.chat.id, 'No article found matching the given problem description.');
             return;
-        } 
+        }
 
         let output = ["We've found the following articles that match your description", ""]
 
         matches.forEach(m => {
-            if(m['measure']>0){
+            if (m['measure'] > 0) {
                 let match = GDPR[m['i']];
                 output.push('*Title: *' + match.name);
                 output.push('*Gdpr Article: *' + match.gdpr_article);
                 output.push("\n");
-            }         
+            }
         })
 
         output.push('\n');
@@ -86,13 +124,10 @@ bot.on('message', message => {
                 ]
             }
         });
-            
-    } else if (message.text.startsWith('/')) {
-        ///Put logic here that answers all possible commands
-        console.log('Omitting message');
-    }
-    else {
-        bot.sendMessage(message.chat.id, 'Please start the bot with /start!');
+    } else {
+        bot.sendMessage(message.chat.id, 'Please start the bot with `/start` or type `/help` for more information', {
+            parse_mode: 'Markdown',
+        });
     }
 });
 
@@ -118,8 +153,11 @@ bot.on("callback_query", callback => {
         let output = [];
         solutions.forEach(solution => {
             output.push('*Title: *' + solution.name);
-            output.push('*Aws solution: *' + solution.aws_solution);
-            output.push("\n");
+            output.push('*Aws solutions: *');
+            solution.aws_solution.forEach(solution => {
+                output.push(' • ' + '[' + solution + '](' + solution + ')');
+            })
+            output.push('\n');
         });
 
         bot.sendMessage(callback.message.chat.id, output.join('\n'), {
